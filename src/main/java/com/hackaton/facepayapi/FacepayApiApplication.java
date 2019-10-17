a45eb961e51168c13ca8839302f5e5f6f35573bd
iroccalada <ignacio.rocca@mercadolibre.com>
2019-10-17 17:03:21 -0300

    Ignacio Rocca | start project

---
+++ src/main/java/com/hackaton/facepayapi/FacepayApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.facepayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacepayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacepayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/facepayapi/FacepayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.facepayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacepayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
