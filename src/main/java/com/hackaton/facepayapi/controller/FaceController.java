c994546efce46b240e8d30e5bc3c9272d0519292
Alexis Sessarego <alexis.sessarego@mercadolibre.com>
2019-10-17 21:27:49 -0300

    fixs

--- src/main/java/com/hackaton/facepayapi/controller/FaceController.java
+++ src/main/java/com/hackaton/facepayapi/controller/FaceController.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.rekognition.model.CreateCollectionRequest;
 import com.amazonaws.services.rekognition.model.CreateCollectionResult;
 import com.hackaton.facepayapi.models.FaceLogin;
-import com.hackaton.facepayapi.service.AddFacesToCollection;
+import com.hackaton.facepayapi.service.AWSFaceRecognition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.Base64;
 import java.util.Optional;
 
 @RestController
 public class FaceController {
 
     @Autowired
-    private AddFacesToCollection addFacesToCollection;
+    private AWSFaceRecognition AWSFaceRecognition;
 
 
     // Only used to add a new face to the collection
@@ -31,7 +27,7 @@ public class FaceController {
     public String faceLogin(@RequestBody FaceLogin login) {
 
         try {
-            return addFacesToCollection.uploadFace(login.getFace());
+            return AWSFaceRecognition.uploadFace(login.getFace());
         }catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,7 +41,7 @@ public class FaceController {
     @GetMapping("/facelogin")
     public ResponseEntity<String> validateLogin(@RequestBody FaceLogin login) {
 
-        Optional<String> faceID = addFacesToCollection.validateFace(login.getFace());
+        Optional<String> faceID = AWSFaceRecognition.validateFace(login.getFace());
         if (!faceID.isPresent()) {
             return ResponseEntity.notFound().build();
         }
@@ -54,24 +50,12 @@ public class FaceController {
     }
 
     // Internal method to create a new collection.
-    public String createCollection(){
-        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard().withRegion("us-east-1").build();//.defaultClient();
-
-        //Replace collectionId with the name of the collection that you want to create.
-
-        String collectionId = "FacePayCollection";
-        System.out.println("Creating collection: " +
-                collectionId );
-
-        CreateCollectionRequest request = new CreateCollectionRequest()
-                .withCollectionId(collectionId);
-
-        CreateCollectionResult createCollectionResult = rekognitionClient.createCollection(request);
-        System.out.println("CollectionArn : " +
-                createCollectionResult.getCollectionArn());
-        System.out.println("Status code : " +
-                createCollectionResult.getStatusCode().toString());
-        return "ok";
+    public ResponseEntity<String> createCollection(){
+        Optional<String> res = AWSFaceRecognition.createCollection();
+        if (!res.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.accepted().body(res.get());
 
     }
 
--- src/main/java/com/hackaton/facepayapi/controller/PaymentController.java
+++ src/main/java/com/hackaton/facepayapi/controller/PaymentController.java
@@ -1,7 +1,7 @@
 package com.hackaton.facepayapi.controller;
 
 import com.hackaton.facepayapi.models.PaymentFrontRequest;
-import com.hackaton.facepayapi.service.AddFacesToCollection;
+import com.hackaton.facepayapi.service.AWSFaceRecognition;
 import com.hackaton.facepayapi.services.PaymentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Base64;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -22,13 +21,13 @@ public class PaymentController {
     @Autowired
     private PaymentsService paymentsService;
     @Autowired
-    private AddFacesToCollection addFacesToCollection;
+    private AWSFaceRecognition AWSFaceRecognition;
 
     @PostMapping(value = "/payments", produces = "application/json")
     public ResponseEntity<String> processPaymentNotification(@RequestBody PaymentFrontRequest request) {
         try {
 
-            Optional<String> faceID = addFacesToCollection.validateFace(request.getImageBase64());
+            Optional<String> faceID = AWSFaceRecognition.validateFace(request.getImageBase64());
             if (!faceID.isPresent()) {
                 return ResponseEntity.notFound().build();
             }
