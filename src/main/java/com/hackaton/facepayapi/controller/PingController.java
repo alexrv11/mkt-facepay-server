1d54a075dd0b7ec015fc59ce73a5708226ca3599
Alex Ventura <alex.rv11@gmail.com>
2019-10-18 03:26:47 -0300

    ping 101

--- src/main/java/com/hackaton/facepayapi/controller/PingController.java
+++ src/main/java/com/hackaton/facepayapi/controller/PingController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
     @RequestMapping("/ping")
     public String ping() {
-        return "pong";
+        return "pong 101";
     }
 }
